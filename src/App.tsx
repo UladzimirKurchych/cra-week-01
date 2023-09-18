5ef57b9527e40b7f1ba2b6052bd6e9254911e86f
UladzimirKurchych <kurchich.v@gmail.com>
2023-09-18 19:41:34 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div>This is APP component</div>
-  );
+    return (
+        <div>
+            This is APP component
+        </div>
+
+    );
+}
+
+function Rating() {
+    return (
+        <div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+        </div>
+    )
+}
+
+function Accordion() {
+    return (
+        <div>
+            <h3>Menu</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
 }
 
 export default App;
