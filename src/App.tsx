324ac83aeae5eb6efd2ebbb307a3d154e8dfd44b
UladzimirKurchych <kurchich.v@gmail.com>
2023-09-18 21:43:23 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-    console.log("App rendering")
-    return (
-        <div>
-            <AppTitle />
-            <Rating/>
-            <Accordion/>
-            <Rating />
-        </div>
-    );
-}
-
-function AppTitle() {
-    return <>This is App component</>
-}
-
 function sayHi() {
     alert("Hello!");
 }
 
 sayHi();
 
+function App() {
+    console.log("App rendering")
+    return (
+        <div>
+            <AppTitle/>
+            <Rating/>
+            <Accordion/>
+            <Rating/>
+        </div>
+    );
+}
+
+function AppTitle() {
+    console.log("AppTitle rendering")
+    return (
+        <>This is App component</>
+    );
+}
+
+
 function Rating() {
     console.log("Rating rendering")
     return (
         <>
-            <Star />
+            <Star/>
         </>
     );
 }
@@ -49,15 +53,30 @@ function Star() {
 function Accordion() {
     console.log("Accordion rendering")
     return (
-        <div>
-            <h3>Menu</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
-        </div>
+        <>
+            <AccordionTitle/>
+            <AccordionBody/>
+        </>
+
     );
 }
+
+function AccordionTitle() {
+    console.log("AccordionTitle rendering")
+    return (
+        <h3>Menu</h3>
+    );
+}
+
+function AccordionBody() {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
+
 
 export default App;
