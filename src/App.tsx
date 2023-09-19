d05cab3b44a6ab8b02f6166e26827917083c55d8
UladzimirKurchych <kurchich.v@gmail.com>
2023-09-19 17:56:41 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -49,33 +49,7 @@ function Star() {
     );
 }
 
-function Accordion() {
-    console.log("Accordion rendering")
-    return (
-        <>
-            <AccordionTitle/>
-            <AccordionBody/>
-        </>
 
-    );
-}
-
-function AccordionTitle() {
-    console.log("AccordionTitle rendering")
-    return (
-        <h3>Menu</h3>
-    );
-}
-
-function AccordionBody() {
-    return (
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
-    )
-}
 
 
 export default App;
