d1974abe3312ff80ea975dbc2e341396888d17ef
UladzimirKurchych <kurchich.v@gmail.com>
2023-09-19 20:02:19 +0200

    Update Rating.tsx

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -3,21 +3,17 @@ import React from "react";
 export function Rating() {
     console.log("Rating rendering")
     return (
-        <>
-            <Star/>
-        </>
+        <div>
+            <Star selected={true}/>
+            <Star selected={true}/>
+            <Star selected={false}/>
+            <Star selectes={false}/>
+            <Star selected={false}/>
+        </div>
     );
 }
 
 function Star() {
     console.log("Star rendering")
-    return (
-        <>
-            <div>star</div>
-            <div>star</div>
-            <div>star</div>
-            <div>star</div>
-            <div>star</div>
-        </>
-    );
+    return <span> <b>star_</b></span>
 }
