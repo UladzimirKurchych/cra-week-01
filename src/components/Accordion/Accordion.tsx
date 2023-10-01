0400a742c65dd81b973baa709339b34995b67507
UladzimirKurchych <kurchich.v@gmail.com>
2023-10-01 18:56:24 +0200

    Update Accordion.tsx

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 
 function Accordion(props: any) {
+    debugger
     return (
-        <>
+        <div>
             <AccordionTitle title={props.titleValue}/>
             <AccordionBody/>
-        </>
+        </div>
 
     );
 }
 
 function AccordionTitle(props: any) {
+    debugger
     return (
         <h3>{props.title}</h3>
     );
