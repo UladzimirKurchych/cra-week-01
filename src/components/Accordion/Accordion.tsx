335a523f2a9d5f102d7982515e0d872bccb1d016
UladzimirKurchych <kurchich.v@gmail.com>
2023-10-01 12:17:51 +0200

    Update Accordion.tsx

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 function Accordion(props: any) {
-    console.log("Accordion rendering")
     return (
         <>
             <AccordionTitle title={props.titleValue}/>
@@ -12,14 +11,12 @@ function Accordion(props: any) {
 }
 
 function AccordionTitle(props: any) {
-    console.log("AccordionTitle rendering")
     return (
         <h3>{props.title}</h3>
     );
 }
 
 function AccordionBody() {
-    console.log("AccordionBody rendering")
     return (
         <ul>
             <li>1</li>
